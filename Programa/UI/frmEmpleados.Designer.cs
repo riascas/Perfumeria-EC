335d11019e30a3c91b728de7192b1fc093b6bfe5
Ruth Iascas <ruth.iascas@gmail.com>
2024-06-24 19:21:51 -0300

    frm cambios

--- Programa/UI/frmEmpleados.Designer.cs
+++ Programa/UI/frmEmpleados.Designer.cs
@@ -33,16 +33,16 @@
             label3 = new System.Windows.Forms.Label();
             label4 = new System.Windows.Forms.Label();
             label5 = new System.Windows.Forms.Label();
-            label6 = new System.Windows.Forms.Label();
-            comboBox6 = new System.Windows.Forms.ComboBox();
-            textBox2 = new System.Windows.Forms.TextBox();
-            textBox3 = new System.Windows.Forms.TextBox();
-            textBox4 = new System.Windows.Forms.TextBox();
-            textBox5 = new System.Windows.Forms.TextBox();
-            label7 = new System.Windows.Forms.Label();
-            textBox6 = new System.Windows.Forms.TextBox();
-            label8 = new System.Windows.Forms.Label();
-            comboBox1 = new System.Windows.Forms.ComboBox();
+            txtNrodocumento = new System.Windows.Forms.TextBox();
+            txtnombrecompleto = new System.Windows.Forms.TextBox();
+            textcorreo = new System.Windows.Forms.TextBox();
+            textclave = new System.Windows.Forms.TextBox();
+            label9 = new System.Windows.Forms.Label();
+            cboRol = new System.Windows.Forms.ComboBox();
+            label10 = new System.Windows.Forms.Label();
+            textconfirmacionclave = new System.Windows.Forms.TextBox();
+            label11 = new System.Windows.Forms.Label();
+            cboEstado = new System.Windows.Forms.ComboBox();
             SuspendLayout();
             // 
             // label1
@@ -51,14 +51,14 @@
             label1.Dock = System.Windows.Forms.DockStyle.Left;
             label1.Location = new System.Drawing.Point(0, 0);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(203, 563);
+            label1.Size = new System.Drawing.Size(223, 563);
             label1.TabIndex = 0;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.BackColor = System.Drawing.Color.White;
-            label2.Location = new System.Drawing.Point(27, 39);
+            label2.Location = new System.Drawing.Point(24, 64);
             label2.Name = "label2";
             label2.Size = new System.Drawing.Size(96, 15);
             label2.TabIndex = 1;
@@ -68,7 +68,7 @@
             // 
             label3.AutoSize = true;
             label3.BackColor = System.Drawing.Color.White;
-            label3.Location = new System.Drawing.Point(26, 109);
+            label3.Location = new System.Drawing.Point(23, 120);
             label3.Name = "label3";
             label3.Size = new System.Drawing.Size(110, 15);
             label3.TabIndex = 2;
@@ -78,7 +78,7 @@
             // 
             label4.AutoSize = true;
             label4.BackColor = System.Drawing.Color.White;
-            label4.Location = new System.Drawing.Point(26, 172);
+            label4.Location = new System.Drawing.Point(22, 177);
             label4.Name = "label4";
             label4.Size = new System.Drawing.Size(46, 15);
             label4.TabIndex = 3;
@@ -88,92 +88,92 @@
             // 
             label5.AutoSize = true;
             label5.BackColor = System.Drawing.Color.White;
-            label5.Location = new System.Drawing.Point(26, 239);
+            label5.Location = new System.Drawing.Point(26, 234);
             label5.Name = "label5";
             label5.Size = new System.Drawing.Size(70, 15);
             label5.TabIndex = 4;
             label5.Text = "Contraseña:";
             // 
-            // label6
+            // txtNrodocumento
             // 
-            label6.AutoSize = true;
-            label6.BackColor = System.Drawing.Color.White;
-            label6.Location = new System.Drawing.Point(26, 314);
-            label6.Name = "label6";
-            label6.Size = new System.Drawing.Size(127, 15);
-            label6.TabIndex = 5;
-            label6.Text = "Confirmar Contraseña:";
+            txtNrodocumento.Location = new System.Drawing.Point(26, 83);
+            txtNrodocumento.Name = "txtNrodocumento";
+            txtNrodocumento.Size = new System.Drawing.Size(167, 23);
+            txtNrodocumento.TabIndex = 13;
             // 
-            // comboBox6
+            // txtnombrecompleto
             // 
-            comboBox6.FormattingEnabled = true;
-            comboBox6.Location = new System.Drawing.Point(30, 404);
-            comboBox6.Name = "comboBox6";
-            comboBox6.Size = new System.Drawing.Size(146, 23);
-            comboBox6.TabIndex = 11;
+            txtnombrecompleto.Location = new System.Drawing.Point(26, 142);
+            txtnombrecompleto.Name = "txtnombrecompleto";
+            txtnombrecompleto.Size = new System.Drawing.Size(167, 23);
+            txtnombrecompleto.TabIndex = 14;
             // 
-            // textBox2
+            // textcorreo
             // 
-            textBox2.Location = new System.Drawing.Point(29, 58);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new System.Drawing.Size(147, 23);
-            textBox2.TabIndex = 13;
+            textcorreo.Location = new System.Drawing.Point(26, 195);
+            textcorreo.Name = "textcorreo";
+            textcorreo.Size = new System.Drawing.Size(166, 23);
+            textcorreo.TabIndex = 15;
             // 
-            // textBox3
+            // textclave
             // 
-            textBox3.Location = new System.Drawing.Point(29, 131);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new System.Drawing.Size(147, 23);
-            textBox3.TabIndex = 14;
+            textclave.Location = new System.Drawing.Point(26, 252);
+            textclave.Name = "textclave";
+            textclave.Size = new System.Drawing.Size(170, 23);
+            textclave.TabIndex = 16;
             // 
-            // textBox4
+            // label9
             // 
-            textBox4.Location = new System.Drawing.Point(30, 190);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new System.Drawing.Size(146, 23);
-            textBox4.TabIndex = 15;
+            label9.AutoSize = true;
+            label9.BackColor = System.Drawing.Color.White;
+            label9.Location = new System.Drawing.Point(23, 288);
+            label9.Name = "label9";
+            label9.Size = new System.Drawing.Size(127, 15);
+            label9.TabIndex = 5;
+            label9.Text = "Confirmar Contraseña:";
             // 
-            // textBox5
+            // cboRol
             // 
-            textBox5.Location = new System.Drawing.Point(26, 257);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new System.Drawing.Size(150, 23);
-            textBox5.TabIndex = 16;
+            cboRol.FormattingEnabled = true;
+            cboRol.Location = new System.Drawing.Point(27, 362);
+            cboRol.Name = "cboRol";
+            cboRol.Size = new System.Drawing.Size(166, 23);
+            cboRol.TabIndex = 11;
             // 
-            // label7
+            // label10
             // 
-            label7.AutoSize = true;
-            label7.BackColor = System.Drawing.Color.White;
-            label7.Location = new System.Drawing.Point(26, 385);
-            label7.Name = "label7";
-            label7.Size = new System.Drawing.Size(27, 15);
-            label7.TabIndex = 17;
-            label7.Text = "Rol:";
+            label10.AutoSize = true;
+            label10.BackColor = System.Drawing.Color.White;
+            label10.Location = new System.Drawing.Point(23, 343);
+            label10.Name = "label10";
+            label10.Size = new System.Drawing.Size(27, 15);
+            label10.TabIndex = 17;
+            label10.Text = "Rol:";
             // 
-            // textBox6
+            // textconfirmacionclave
             // 
-            textBox6.Location = new System.Drawing.Point(29, 332);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new System.Drawing.Size(147, 23);
-            textBox6.TabIndex = 18;
+            textconfirmacionclave.Location = new System.Drawing.Point(26, 306);
+            textconfirmacionclave.Name = "textconfirmacionclave";
+            textconfirmacionclave.Size = new System.Drawing.Size(167, 23);
+            textconfirmacionclave.TabIndex = 18;
             // 
-            // label8
+            // label11
             // 
-            label8.AutoSize = true;
-            label8.BackColor = System.Drawing.Color.White;
-            label8.Location = new System.Drawing.Point(30, 463);
-            label8.Name = "label8";
-            label8.Size = new System.Drawing.Size(45, 15);
-            label8.TabIndex = 19;
-            label8.Text = "Estado:";
+            label11.AutoSize = true;
+            label11.BackColor = System.Drawing.Color.White;
+            label11.Location = new System.Drawing.Point(26, 403);
+            label11.Name = "label11";
+            label11.Size = new System.Drawing.Size(45, 15);
+            label11.TabIndex = 19;
+            label11.Text = "Estado:";
             // 
-            // comboBox1
+            // cboEstado
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new System.Drawing.Point(30, 481);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new System.Drawing.Size(146, 23);
-            comboBox1.TabIndex = 20;
+            cboEstado.FormattingEnabled = true;
+            cboEstado.Location = new System.Drawing.Point(26, 421);
+            cboEstado.Name = "cboEstado";
+            cboEstado.Size = new System.Drawing.Size(166, 23);
+            cboEstado.TabIndex = 20;
             // 
             // frmEmpleados
             // 
@@ -181,16 +181,16 @@
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             BackColor = System.Drawing.Color.Black;
             ClientSize = new System.Drawing.Size(876, 563);
-            Controls.Add(comboBox1);
-            Controls.Add(label8);
-            Controls.Add(textBox6);
-            Controls.Add(label7);
-            Controls.Add(textBox5);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(comboBox6);
-            Controls.Add(label6);
+            Controls.Add(cboEstado);
+            Controls.Add(label11);
+            Controls.Add(textconfirmacionclave);
+            Controls.Add(label10);
+            Controls.Add(textclave);
+            Controls.Add(textcorreo);
+            Controls.Add(txtnombrecompleto);
+            Controls.Add(cboRol);
+            Controls.Add(txtNrodocumento);
+            Controls.Add(label9);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
@@ -209,15 +209,15 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox comboBox6;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtNrodocumento;
+        private System.Windows.Forms.TextBox txtnombrecompleto;
+        private System.Windows.Forms.TextBox textcorreo;
+        private System.Windows.Forms.TextBox textclave;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cboRol;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textconfirmacionclave;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox cboEstado;
     }
 }
